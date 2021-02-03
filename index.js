a3413559c2bc926660d11efb36e9f88584e16993
JCJ <jesse.jackson.c@gmail.com>
2021-02-03 12:41:08 -0600

    Adding comments

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 require("./services/passport");
 
+// storing express methods into variable
 const app = express();
 
 // returning authRoutes function and immediately invoking app parameter
 require("./routes/authRoutes")(app);
 
+// allowing heroku to find an available port or localhost5000
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- services/passport.js
+++ services/passport.js
@@ -2,6 +2,10 @@ const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
 
+// each instance of utilizing Google OAuth
+// retrieving client ID and Secret
+// when data is received, send user back to callbackURL
+// second argument is pulling data from newly authorized user
 passport.use(
   new GoogleStrategy(
     {
