9712067a16ef2c7d7be084d13ffc87a0ecc53beb
JCJ <jesse.jackson.c@gmail.com>
2021-02-07 16:30:13 -0600

    Importing Provider and Redux store, creating a template store

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
 
 import App from "./components/App";
+
+// Redux store that is accessible to every component
+const store = createStore(() => [], {}, applyMiddleware());
 
 ReactDOM.render(<App />, document.querySelector("#root"));
