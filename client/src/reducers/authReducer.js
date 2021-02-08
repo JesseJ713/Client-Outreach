1657aa5430bf0f1814331bb97d95e0ccd20dc8e6
JCJ <jesse.jackson.c@gmail.com>
2021-02-08 12:21:08 -0600

    Setting up base reducer for authentication

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,8 @@
+// accepting state and action of authentication as arguments
+// returns next state of application
+export default function (state = {}, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
