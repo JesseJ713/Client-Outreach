cc9311d95eff8bb37028a94706f9172bdb9c132d
JCJ <jesse.jackson.c@gmail.com>
2021-02-11 17:31:45 -0600

    Passing in reduxThunk as parameter to middleware

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
 
 import App from "./components/App";
 import reducers from "./reducers";
 
 // Redux store that is accessible to every component
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 // Provider component houses the store at top level
 // App child will have access to Redux store
