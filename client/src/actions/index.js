c40ef515c97b7967a43bdf9198b3166221d35277
JCJ <jesse.jackson.c@gmail.com>
2021-02-11 17:52:03 -0600

    Creating action creator for current user api

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,13 @@
 import axios from "axios";
 import { FETCH_USER } from "./types";
 
+// returning function
+// redux thunk will see function and call with a dispatch
+// waiting until response from api to complete dispatch
 const fetchUser = () => {
-  axios.get("/api/current_user");
+  return function (dispatch) {
+    axios
+      .get("/api/current_user")
+      .then((res) => dispatch({ type: FETCH_USER, payload: res }));
+  };
 };
