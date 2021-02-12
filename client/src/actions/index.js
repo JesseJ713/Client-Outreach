8cbb215320fbd75c1700d1f4f7681857f62e5cfa
JCJ <jesse.jackson.c@gmail.com>
2021-02-11 18:08:33 -0600

    Included export keyword, successful test of fetch user action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,7 +4,7 @@ import { FETCH_USER } from "./types";
 // returning function
 // redux thunk will see function and call with a dispatch
 // waiting until response from api to complete dispatch
-const fetchUser = () => {
+export const fetchUser = () => {
   return function (dispatch) {
     axios
       .get("/api/current_user")
