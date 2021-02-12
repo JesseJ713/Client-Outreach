264c00a118e14756241ef82009ed245687c6a1dd
JCJ <jesse.jackson.c@gmail.com>
2021-02-11 18:03:44 -0600

    Logging actions being taken

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,7 @@
 // accepting state and action of authentication as arguments
 // returns next state of application
 export default function (state = {}, action) {
+  console.log(action);
   switch (action.type) {
     default:
       return state;
