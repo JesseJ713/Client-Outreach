1eea83b9f9e420ddde4b46ad170fd8a2950afad9
JCJ <jesse.jackson.c@gmail.com>
2021-02-14 17:50:58 -0600

    imported handleToken action and utilized within token property as callback

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import StripeCheckout from "react-stripe-checkout";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 // amount in US cents
 // token is callback function with token received from Stripe
@@ -11,7 +13,7 @@ class Payments extends Component {
         name="Client Outreach"
         description="$5 for 5 email credits"
         amount={500}
-        token={(token) => console.log(token)}
+        token={(token) => this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
       >
         <button className="btn">Add Credits</button>
@@ -20,4 +22,4 @@ class Payments extends Component {
   }
 }
 
-export default Payments;
+export default connect(null, actions)(Payments);
