d28919e5dbed4cfb75003939832a8c6a72bdc5ff
JCJ <jesse.jackson.c@gmail.com>
2021-02-15 15:22:41 -0600

    Installing body-parser middleware and envoking

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 require("./models/User");
 require("./services/passport");
@@ -11,6 +12,7 @@ mongoose.connect(keys.mongoURI);
 // storing express methods into variable
 const app = express();
 
+app.use(bodyParser.json());
 // telling express to use cookies
 // cookies have a maxAge of 30 days
 // cookies have random encryption from config
