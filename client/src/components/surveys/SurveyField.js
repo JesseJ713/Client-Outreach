19d0c16d52e0d4ec7c5db2a4d79fe87f4400ec39
JCJ <jesse.jackson.c@gmail.com>
2021-03-20 15:24:54 -0500

    Setting up helper function for fields so that custom fields can be added upon redux forms

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,12 @@
+// SurveyField contains logic to render
+// labels and text input
+
+import React from "react";
+
+export default () => {
+  return (
+    <div>
+      <input />
+    </div>
+  );
+};
