c52d93ae3da9b85baf442ad4c74fa45a2b27cc94
JCJ <jesse.jackson.c@gmail.com>
2021-03-21 19:58:04 -0500

    Recoding the last date given feedback within Mongo

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -69,6 +69,7 @@ module.exports = (app) => {
           {
             $inc: { [choice]: 1 },
             $set: { "recipients.$.responded": true },
+            lastResponded: new Date(),
           }
         ).exec();
       })
